3d5206b78c6f9eecdddf1bb9f680d35daca93acb
javandrex <jgarcia@ubicacuenca.com>
2020-02-28 12:58:27 -0500

    conftest clean

--- app/conftest.py
+++ app/conftest.py
@@ -7,32 +7,3 @@ from app.__init__ import create_app
 def app():
     app = create_app()
     return app
-
-
-
-
-# import pytest
-# from app.__init__ import create_app
-
-# myapp = create_app()
-# print(myapp)
-# # Creates a fixture whose name is "app"
-# # and returns our flask server instance
-# @pytest.fixture
-# def app():
-#     app = create_app()
-#     a = 0 / 1
-#     #app = {}
-#     return app
-
-
-# import pytest
-# from flask import Flask
-# from app import initialize_app
-
-
-# @pytest.fixture
-# def app():
-#     app = Flask(__name__)
-#     initialize_app(app)
-#     return app    
